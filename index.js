c17163179d4714de4c68fb658b67a6806b4fb805
Cédrick ALEXANDRE <alexandre.cedrick@gmail.com>
2016-08-11 16:18:54 +0200

    clean

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ firstJson.forEach(function(value, key) {
     var re = /"/g;
     var str = value.body;
     value.body = str.replace(re, '\\"');
-    console.log(value.body);
   }
 
   fs.writeFileSync('composant/' + value.composant  + '_' + value._id +  '.json', compiled(value) , {encoding: 'utf8'})
