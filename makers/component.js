5bc11672c53a5def066bb3746b57a7b27df3eaab
Cédrick ALEXANDRE <alexandre.cedrick@gmail.com>
2016-09-26 16:42:10 +0200

    Ajout de retour visuel

--- makers/component.js
+++ makers/component.js
@@ -2,9 +2,14 @@ const _ = require('lodash')
 
 const fs = require('fs')
 
+// For colors
+const chalk = require('chalk');
+
+// Tools
 const setParentId = require('./../tools/setParentId')
 const checkIfKeyExit = require('./../tools/checkIfKeyExit')
 const cleanText = require('./../tools/cleanText')
+const checkFileExistsSync = require('./../tools/checkFileExistsSync')
 
 function makeComponent (value, directory, component_result) {
   var file = fs.readFileSync('model/' + value.composant + '.json', 'utf-8')
@@ -15,15 +20,6 @@ function makeComponent (value, directory, component_result) {
   value._parentId = setParentId(value._id)
   value.body = cleanText(value.body)
 
-  // console.log( value.composant);
-  if(value.composant === 'multicam') {
-    // console.log(value._medias[0]);
-    value.media1_title = cleanText(value.media1_title)
-    value.media2_title = cleanText(value.media2_title)
-    value.media3_title = cleanText(value.media3_title)
-    value.media4_title = cleanText(value.media4_title)
-  }
-
   // On n'a pas besoin de récupérer le tableau après la fonction car envoi par référence
   checkIfKeyExit(value)
 
@@ -31,6 +27,16 @@ function makeComponent (value, directory, component_result) {
 
   // On insère les valeur s et on écrit le fichier
   fs.writeFileSync('result/' + directory + '/' +value.composant  + '_' + value._id +  '.json', compiled(value) , {encoding: 'utf8'})
+
+
+  if( checkFileExistsSync('result/' + directory + '/' +value.composant  + '_' + value._id +  '.json') ) {
+    console.log('File ' + chalk.blue(value.composant  + '_' + value._id +  '.json') + chalk.green(' was created') )
+  }
+  else {
+    console.log(chalk.red('Error: File ' + chalk.yellow(value.composant  + '_' + value._id +  '.json') + ' wasn\'t created'));
+  }
+
+
 }
 
 module.exports = makeComponent
